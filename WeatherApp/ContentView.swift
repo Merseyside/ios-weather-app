86a812d68b9d563fc7afcae6582779675bb36609
Ivan Sablin <ivanklessablin@gmail.com>
2020-11-09 15:03:25 +0700

    Initial Commit

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Ivan on 09.11.2020.
+//  Copyright © 2020 Ivan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
